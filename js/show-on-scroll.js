4372c646290404c4a0a3d6ea454c0d0f83e780a5
Kota1709 <165947150+Kota1709@users.noreply.github.com>
2024-04-16 13:26:22 -0400

    Add files via upload

---
+++ js/show-on-scroll.js
@@ -0,0 +1,76 @@
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+var revealThreshold = window.innerHeight * .99; // Adjust the threshold as needed
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element, revealThreshold)) {
+      element.classList.add('is-visible');
+
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el, threshold) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+const fadeInOut = document.querySelector('.fade-in-out');
+
+function fadeIn() {
+  fadeInOut.classList.add('fade-in');
+}
+
+
+function fadeOut() {
+  fadeInOut.classList.remove('fade-in');
+}
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  if (isInViewport(fadeInOut)) {
+    fadeIn();
+  } else {
+    fadeOut();
+  }
+}
+
+/*fade-out-effect*/
+
+
+
